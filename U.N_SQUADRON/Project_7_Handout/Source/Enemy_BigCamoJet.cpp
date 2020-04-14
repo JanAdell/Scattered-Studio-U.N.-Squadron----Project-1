792c247fefd373911c6fe9e1b6bfd6f36823de49
JanAdell <36292051+JanAdell@users.noreply.github.com>
2020-04-14 12:47:26 +0200

    Filled enemies with handout given material
    Incoming commits defining animations and pathing

---
+++ U.N_SQUADRON/Project_7_Handout/Source/Enemy_BigCamoJet.cpp
@@ -0,0 +1,26 @@
+#include "Enemy_BigCamoJet.h"
+
+#include "Application.h"
+#include "ModuleCollisions.h"
+
+Enemy_BigCamoJet::Enemy_BigCamoJet(int x, int y) : Enemy(x, y)
+{
+	/*fly.PushBack({ 5,72,21,22 });
+	currentAnim = &fly;
+	fly.speed = 0.01f;
+
+	// TODO 3: Have the Brown Cookies describe a path in the screen
+	path.PushBack({ -0.8f , -0.5f }, 100, &fly);
+	path.PushBack({ -0.8f , 0.5f }, 100, &fly);
+	collider = App->collisions->AddCollider({ 0, 0, 24, 24 }, Collider::Type::ENEMY, (Module*)App->enemies);*/
+}
+
+void Enemy_BigCamoJet::Update()
+{
+	/*path.Update();
+	position = spawnPos + path.GetRelativePosition();
+
+	// Call to the base class. It must be called at the end
+	// It will update the collider depending on the position
+	Enemy::Update();*/
+}
---
+++ U.N_SQUADRON/Project_7_Handout/Source/Enemy_OrangeJet.cpp
@@ -0,0 +1,26 @@
+#include "Enemy_OrangeJet.h"
+
+#include "Application.h"
+#include "ModuleCollisions.h"
+
+Enemy_OrangeJet::Enemy_OrangeJet(int x, int y) : Enemy(x, y)
+{
+	/*fly.PushBack({ 5,72,21,22 });
+	currentAnim = &fly;
+	fly.speed = 0.01f;
+
+	// TODO 3: Have the Brown Cookies describe a path in the screen
+	path.PushBack({ -0.8f , -0.5f }, 100, &fly);
+	path.PushBack({ -0.8f , 0.5f }, 100, &fly);
+	collider = App->collisions->AddCollider({ 0, 0, 24, 24 }, Collider::Type::ENEMY, (Module*)App->enemies);*/
+}
+
+void Enemy_OrangeJet::Update()
+{
+	/*path.Update();
+	position = spawnPos + path.GetRelativePosition();
+
+	// Call to the base class. It must be called at the end
+	// It will update the collider depending on the position
+	Enemy::Update();*/
+}
